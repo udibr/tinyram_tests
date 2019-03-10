6cb68418c41d21a167dc62316b6225446d44beb9
Ehud Ben-Reuven <udi@benreuven.com>
2019-03-09 19:34:54 -0500

    fix error checking

--- examples.sh
+++ examples.sh
@@ -5,9 +5,9 @@ cd libsnark/build
 cmake ..
 make demo_ram_ppzksnark
 cd ../..
-if ! yes '' | libsnark/build/libsnark/demo_ram_ppzksnark --assembly simple.s --processed_assembly simple.proc --architecture_params simple.params --computation_bounds simple.bounds --primary_input simple.input --auxiliary_input simple.input; then
+if yes '' | libsnark/build/libsnark/demo_ram_ppzksnark --assembly simple.s --processed_assembly simple.proc --architecture_params simple.params --computation_bounds simple.bounds --primary_input simple.input --auxiliary_input simple.input | grep "The verification result is: FAIL"; then
     echo "Unexpected fail!" >&2
-elif yes '' | libsnark/build/libsnark/demo_ram_ppzksnark --assembly simple.s --processed_assembly simple.proc --architecture_params simple.params --computation_bounds simple.bounds --primary_input simple.input --auxiliary_input simple.false_input; then
+elif yes '' | libsnark/build/libsnark/demo_ram_ppzksnark --assembly simple.s --processed_assembly simple.proc --architecture_params simple.params --computation_bounds simple.bounds --primary_input simple.input --auxiliary_input simple.false_input | grep "The verification result is: PASS"; then
     echo "Unexpected success!" >&2
 else
     echo "All clear!"
